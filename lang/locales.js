863e4899494058394d271879b0a099cae5f035ec
AmirDev <benhammouda.amir@gmail.com>
2023-01-20 18:25:31 +0100

    minor text fix

--- lang/locales.js
+++ lang/locales.js
@@ -4,7 +4,7 @@ const locales = {
 	subtitle: { fr: '19 ème', en: 'XIX ctry', ru: '19 ème', cn: '19 ème' },
 	menu1: { fr: 'Collection', en: 'Collection', ru: 'Collection', cn: 'Collection' },
 	menu2: { fr: 'Artistes', en: 'Artists', ru: 'Artists', cn: 'Artists' },
-	menu3: { fr: 'Vente au Musée', en: 'In Museum', ru: 'Vente au Musée', cn: 'Vente au Musée' },
+	menu3: { fr: 'Musée', en: 'In Museum', ru: 'Musée', cn: 'Musée' },
 	menu4: { fr: 'Vente Notable', en: 'Notable Sales', ru: 'Vente Notable', cn: 'Vente Notable' },
 	menu5: { fr: 'Galerie A.Judic', en: 'A.Judic Gallery', ru: 'Galerie A.Judic', cn: 'Galerie A.Judic' },
 	menu6: { fr: 'Contact', en: 'Contact us', ru: 'Contact', cn: 'Contact' },
