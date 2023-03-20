88f7d4367cb713196c28fdb3b8ed401bdaff5322
AmirDev <benhammouda.amir@gmail.com>
2023-03-20 12:39:08 +0100

    playsInline for IOS

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,27 @@
-import { useState, useRef, useEffect } from 'react';
-import { useGlobalContext } from '../components/GlobalContext';
-import { Video, CloudinaryContext } from 'cloudinary-react';
+import { useState, useRef, useEffect } from "react"
+import { useGlobalContext } from "../components/GlobalContext"
+import { Video, CloudinaryContext } from "cloudinary-react"
 
-import Head from 'next/head';
-import Image from 'next/image';
-import { MdVolumeUp } from 'react-icons/md';
-import Link from 'next/link';
+import Head from "next/head"
+import Image from "next/image"
+import { MdVolumeUp } from "react-icons/md"
+import Link from "next/link"
 
-import { sanityClient } from '../lib/sanityClient';
-import imageUrlBuilder from '@sanity/image-url';
-import locales from '../lang/locales.js';
-import Nav from '../components/Nav';
-import NavBar from '../components/NavBar';
-import MobNav from '../components/MobNav';
-import poster from '../public/poster-home.png';
+import { sanityClient } from "../lib/sanityClient"
+import imageUrlBuilder from "@sanity/image-url"
+import locales from "../lang/locales.js"
+import Nav from "../components/Nav"
+import NavBar from "../components/NavBar"
+import MobNav from "../components/MobNav"
+import poster from "../public/poster-home.png"
 
 export default function Home({ walls }) {
-	const { lang, setLang } = useGlobalContext();
+	const { lang, setLang } = useGlobalContext()
 
-	const imageBuilder = imageUrlBuilder({ projectId: 'r1wp5yv2', dataset: 'production' });
+	const imageBuilder = imageUrlBuilder({ projectId: "r1wp5yv2", dataset: "production" })
 	const urlFor = (source) => {
-		return imageBuilder.image(source);
-	};
+		return imageBuilder.image(source)
+	}
 
 	/* 	const [currentSlide, setCurrentSlide] = useState(0);
 	let sliderInterval = useRef();
@@ -43,34 +43,34 @@ export default function Home({ walls }) {
 		<div>
 			<Head>
 				<title>{locales.title[lang]}</title>
-				<meta name='description' content={locales.desc[lang]} />
+				<meta name="description" content={locales.desc[lang]} />
 			</Head>
-			<main className=''>
-				<CloudinaryContext cloud_name='amircloud'>
-					<Video className='h-screen w-full object-cover' publicId='marc/home' autoPlay muted loop poster={poster} />
+			<main className="">
+				<CloudinaryContext cloud_name="amircloud">
+					<Video className="h-screen w-full object-cover" publicId="marc/home" autoPlay playsInline muted loop poster={poster} />
 				</CloudinaryContext>
 				{/* 	<div className='h-screen -z-10 absolute overflow-hidden'>
 		
 
 					<Image src={urlFor(walls[currentSlide].image).quality(100).url()} className='anim -z-10 object-cover h-screen w-screen' alt='bg' width='2500' height='2500' />;
 				</div> */}
-				<div className='md:hidden'>
+				<div className="md:hidden">
 					<NavBar />
 				</div>
-				<nav className='hidden md:block w-[320px] absolute text-white z-10 top-12 left-12'>
+				<nav className="hidden md:block w-[320px] absolute text-white z-10 top-12 left-12">
 					<Nav />
 				</nav>
 			</main>
 		</div>
-	);
+	)
 }
 
 export const getServerSideProps = async () => {
-	const walls = await sanityClient.fetch(`*[_type == "walls"]`);
+	const walls = await sanityClient.fetch(`*[_type == "walls"]`)
 
 	return {
 		props: {
 			walls,
 		},
-	};
-};
+	}
+}
