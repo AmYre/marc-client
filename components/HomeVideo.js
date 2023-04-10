0c7669a5847950b072e74acc905e48c9f7e0129c
AmirDev <benhammouda.amir@gmail.com>
2023-04-11 01:23:45 +0200

    fix component display name

--- components/HomeVideo.js
+++ components/HomeVideo.js
@@ -2,7 +2,7 @@ import React, { memo } from "react"
 
 import { Video, CloudinaryContext } from "cloudinary-react"
 
-const MyVideo = memo(() => {
+const HomeVideo = memo(() => {
 	return (
 		<CloudinaryContext cloud_name="amircloud" secure={true}>
 			<Video className="h-screen w-full object-cover" publicId="marc/home" autoPlay playsInline muted loop poster={{ startOffset: "0" }} />
@@ -10,4 +10,4 @@ const MyVideo = memo(() => {
 	)
 })
 
-export default MyVideo
+export default HomeVideo
