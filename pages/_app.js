eae7ee830e01da24394278d764c113d565472308
AmirDev <benhammouda.amir@gmail.com>
2023-05-25 20:35:24 +0200

    loading progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
-import '../styles/globals.css';
-import '../styles/custom.css';
-import { ContextProvider } from '../components/GlobalContext';
+import "../styles/globals.css"
+import "../styles/custom.css"
+import { ContextProvider } from "../components/GlobalContext"
+import NextNProgress from "nextjs-progressbar"
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ContextProvider>
+			<NextNProgress color="#6A2147" />
 			<Component {...pageProps} />
 		</ContextProvider>
-	);
+	)
 }
 
-export default MyApp;
+export default MyApp
