dd8ed77790bc2302f45f086937ab127be9a5feea
AmirDev <benhammouda.amir@gmail.com>
2023-06-16 09:27:06 +0200

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/globals.css"
 import "../styles/custom.css"
 import { ContextProvider } from "../components/GlobalContext"
+import { Analytics } from "@vercel/analytics/react"
+
 import NextNProgress from "nextjs-progressbar"
 
 function MyApp({ Component, pageProps }) {
@@ -8,6 +10,7 @@ function MyApp({ Component, pageProps }) {
 		<ContextProvider>
 			<NextNProgress color="#6A2147" />
 			<Component {...pageProps} />
+			<Analytics />
 		</ContextProvider>
 	)
 }
