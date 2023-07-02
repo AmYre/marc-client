13b6c82007935221c211bc668ae859db463d58cf
AmirDev <benhammouda.amir@gmail.com>
2023-07-02 04:46:28 +0200

    creating PWA

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("next-pwa")({
+	dest: "public",
+	register: true,
+	sw: "service-worker.js",
+})
+
+const nextConfig = withPWA({
 	reactStrictMode: false,
 	swcMinify: true,
 	images: {
 		domains: ["cdn.sanity.io", "marc.sanity.studio", "v.calameo.com", "calameo.com", "res.cloudinary.com"],
 	},
-}
+})
 
 module.exports = nextConfig
--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
 			<Html lang="fr">
 				<Head>
 					<link rel="icon" href="/favicon.ico" />
-					<link rel="alternate" hreflang="fr" href="https://marcmaison.art/" />
+					<link rel="alternate" hrefLang="fr" href="https://marcmaison.art/" />
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 					<link href="https://fonts.googleapis.com/css2?family=Libre+Bodoni:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
@@ -15,6 +15,9 @@ class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
 						rel="stylesheet"
 					/>
+					<link rel="manifest" href="/manifest.json" />
+					<link rel="apple-touch-icon" href="/icon.png"></link>
+					<meta name="theme-color" content="#fff" />
 				</Head>
 				<body>
 					<Main />
