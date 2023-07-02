e199a767fceef1a9e3b99bfba51f600249d68c93
AmirDev <benhammouda.amir@gmail.com>
2023-07-02 03:40:34 +0200

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "../styles/globals.css"
 import "../styles/custom.css"
 import { ContextProvider } from "../components/GlobalContext"
 import { Analytics } from "@vercel/analytics/react"
-
 import NextNProgress from "nextjs-progressbar"
 
 function MyApp({ Component, pageProps }) {
