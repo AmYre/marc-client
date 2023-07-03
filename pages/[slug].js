5812e9372cbc589e21f3befb5e73c8a5c308f424
AmirDev <benhammouda.amir@gmail.com>
2023-07-03 18:09:02 +0200

    textes above

--- pages/[slug].js
+++ pages/[slug].js
@@ -242,11 +242,20 @@ const DetailProduct = () => {
 			<div className="hidden lg:block">
 				<div className="flex gap-3 absolute bottom-10 right-10 items-center">
 					{!playing && (
-						<svg className="drarrow absolute bottom-0 right-32 scale-[.6]" version="1.1" x="0px" y="0px" viewBox="0 0 43.1 85.9">
-							<path strokeLinecap="round" strokeLinejoin="round" className="st0 draw-arrow" d="M11.3,2.5c-5.8,5-8.7,12.7-9,20.3s2,15.1,5.3,22c6.7,14,18,25.8,31.7,33.1" />
-							<path strokeLinecap="round" strokeLinejoin="round" className="draw-arrow tail-1" d="M40.6,78.1C39,71.3,37.2,64.6,35.2,58" />
-							<path strokeLinecap="round" strokeLinejoin="round" className="draw-arrow tail-2" d="M39.8,78.5c-7.2,1.7-14.3,3.3-21.5,4.9" />
-						</svg>
+						<div className="absolute bottom-0 right-0">
+							<p className="absolute right-[200px] bottom-[160px] text-white text-center text-xl font-bodoni">Son</p>
+
+							<svg className="drarrow drastroke absolute bottom-0 right-32 scale-[.6]" version="1.1" x="0px" y="0px" viewBox="0 0 43.1 85.9">
+								<path strokeLinecap="round" strokeLinejoin="round" className="st0 draw-arrow" d="M11.3,2.5c-5.8,5-8.7,12.7-9,20.3s2,15.1,5.3,22c6.7,14,18,25.8,31.7,33.1" />
+								<path strokeLinecap="round" strokeLinejoin="round" className="draw-arrow tail-1" d="M40.6,78.1C39,71.3,37.2,64.6,35.2,58" />
+								<path strokeLinecap="round" strokeLinejoin="round" className="draw-arrow tail-2" d="M39.8,78.5c-7.2,1.7-14.3,3.3-21.5,4.9" />
+							</svg>
+							<svg className="drarrow absolute bottom-0 right-32  scale-[.6]" version="1.1" x="0px" y="0px" viewBox="0 0 43.1 85.9">
+								<path strokeLinecap="round" strokeLinejoin="round" className="st0 draw-arrow" d="M11.3,2.5c-5.8,5-8.7,12.7-9,20.3s2,15.1,5.3,22c6.7,14,18,25.8,31.7,33.1" />
+								<path strokeLinecap="round" strokeLinejoin="round" className="draw-arrow tail-1" d="M40.6,78.1C39,71.3,37.2,64.6,35.2,58" />
+								<path strokeLinecap="round" strokeLinejoin="round" className="draw-arrow tail-2" d="M39.8,78.5c-7.2,1.7-14.3,3.3-21.5,4.9" />
+							</svg>
+						</div>
 					)}
 					<div className="flex flex-col">
 						<motion.button
@@ -265,7 +274,6 @@ const DetailProduct = () => {
 								</div>
 							)}
 						</motion.button>
-						<p className="absolute bottom-[-30px] left-2 text-white text-center text-xl font-bodoni">Son</p>
 					</div>
 					<div className="flex flex-col">
 						<div className="w-[50px] h-[50px] m-auto bg-yellow rounded-full border-2 border-white p-2 opacity-95 flex flex-col items-center justify-center">
@@ -281,6 +289,22 @@ const DetailProduct = () => {
 						</div>
 					</div>
 					<div className="flex flex-col">
+						{!translate && (
+							<div>
+								<div className="absolute bottom-[145px] right-[30px] text-white text-center text-xl font-bodoni">Langues</div>
+
+								<svg className="drarrow drastroke absolute bottom-0 right-[-40px] scale-[.4]" version="1.1" x="0px" y="0px" viewBox="0 0 43.1 85.9">
+									<path strokeLinecap="round" strokeLinejoin="round" className="st0 draw-arrow" d="M11.3,2.5c-5.8,5-8.7,12.7-9,20.3s2,15.1,5.3,22c6.7,14,18,25.8,31.7,33.1" />
+									<path strokeLinecap="round" strokeLinejoin="round" className="draw-arrow tail-1" d="M40.6,78.1C39,71.3,37.2,64.6,35.2,58" />
+									<path strokeLinecap="round" strokeLinejoin="round" className="draw-arrow tail-2" d="M39.8,78.5c-7.2,1.7-14.3,3.3-21.5,4.9" />
+								</svg>
+								<svg className="drarrow absolute bottom-0 right-[-40px] scale-[.4]" version="1.1" x="0px" y="0px" viewBox="0 0 43.1 85.9">
+									<path strokeLinecap="round" strokeLinejoin="round" className="st0 draw-arrow" d="M11.3,2.5c-5.8,5-8.7,12.7-9,20.3s2,15.1,5.3,22c6.7,14,18,25.8,31.7,33.1" />
+									<path strokeLinecap="round" strokeLinejoin="round" className="draw-arrow tail-1" d="M40.6,78.1C39,71.3,37.2,64.6,35.2,58" />
+									<path strokeLinecap="round" strokeLinejoin="round" className="draw-arrow tail-2" d="M39.8,78.5c-7.2,1.7-14.3,3.3-21.5,4.9" />
+								</svg>
+							</div>
+						)}
 						<div
 							className="w-[50px] h-[50px] m-auto bg-yellow rounded-full border-2 p-2 opacity-95 flex items-center justify-center"
 							onClick={() => {
@@ -288,10 +312,9 @@ const DetailProduct = () => {
 							}}>
 							<BsTranslate className="text-2xl text-white hover:scale-110 transition-all duration-300 cursor-pointer" />
 						</div>
-						<div className="absolute bottom-[-30px] right-[-7px] text-white text-center text-xl font-bodoni">Langues</div>
 					</div>
 					<AnimatePresence>
-						{!translate && (
+						{translate && (
 							<motion.div
 								className="absolute left-0 bottom-[80px] p-4 flex flex-wrap justify-center items-center gap-3 bg-layout bg-opacity-70"
 								initial={{ x: "10px", opacity: 0 }}
