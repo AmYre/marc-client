b158c09ff90c09a85812bb0281d6bfc62c4541a9
AmYre <benhammouda.amir@gmail.com>
2023-07-20 17:29:17 +0200

    image product in form

--- components/Contact.js
+++ components/Contact.js
@@ -1,49 +1,49 @@
-import React, { useState, useRef } from "react"
-import { useSearchParams } from "next/navigation"
+import React, { useState, useRef } from "react";
+import { useSearchParams } from "next/navigation";
 
-import Image from "next/image"
-import { useGlobalContext } from "../components/GlobalContext"
-import imageUrlBuilder from "@sanity/image-url"
-import { MutatingDots } from "react-loader-spinner"
-import TextField from "@mui/material/TextField"
-import { motion } from "framer-motion"
-import emailjs from "@emailjs/browser"
+import Image from "next/image";
+import { useGlobalContext } from "../components/GlobalContext";
+import imageUrlBuilder from "@sanity/image-url";
+import { MutatingDots } from "react-loader-spinner";
+import TextField from "@mui/material/TextField";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
-import contactPic from "../public/contact.png"
+import contactPic from "../public/contact.png";
 
 const Contact = () => {
-	const { lang, setLang, texts, setTexts, currentProduct, setCurrentProduct } = useGlobalContext()
+	const { lang, setLang, texts, setTexts, currentProduct, setCurrentProduct } = useGlobalContext();
 
-	const [sent, setSent] = useState(false)
-	const [delay, setDelay] = useState(false)
-	const form = useRef()
+	const [sent, setSent] = useState(false);
+	const [delay, setDelay] = useState(false);
+	const form = useRef();
 
-	const params = useSearchParams()
-	const onDemand = params.get("ask")
+	const params = useSearchParams();
+	const onDemand = params.get("ask");
 
 	const sendEmail = (e) => {
-		setSent(true)
-		setDelay(true)
+		setSent(true);
+		setDelay(true);
 		setTimeout(() => {
-			setDelay(false)
-		}, 2000)
-		e.preventDefault()
+			setDelay(false);
+		}, 2000);
+		e.preventDefault();
 
 		emailjs.sendForm("service_kmun1ds", "template_kzoezx5", form.current, "mI6zt6KbO8qA65ye9").then(
 			(result) => {
-				console.log("Mail sent : ", result.text)
+				console.log("Mail sent : ", result.text);
 			},
 			(error) => {
-				console.log("Mail error : ", error.text)
+				console.log("Mail error : ", error.text);
 			}
-		)
-	}
+		);
+	};
 
-	const imageBuilder = imageUrlBuilder({ projectId: "r1wp5yv2", dataset: "production" })
+	const imageBuilder = imageUrlBuilder({ projectId: "r1wp5yv2", dataset: "production" });
 
 	const urlFor = (source) => {
-		return imageBuilder.image(source)
-	}
+		return imageBuilder.image(source);
+	};
 
 	return (
 		<div className="p-12 pt-28 md:pt-12">
@@ -75,6 +75,7 @@ const Contact = () => {
 					</div>
 					<TextField className="w-full max-w-[800px]" name="message" id="standard-textarea" label={texts.formMessage[lang]} multiline variant="standard" />
 					<input className="hidden" type="hidden" name="ref" value={currentProduct?.title[lang] || currentProduct?.title.en} />
+					<input className="hidden" type="hidden" name="pic" value={onDemand ? urlFor(currentProduct?.image)?.url() : "nopic"} />
 					<input
 						className="bg-secondary w-[200px] hover:cursor-pointer hover:scale-[1.02] hover:shadow-md text-white px-8 py-4 rounded transition-all duration-300"
 						type="submit"
@@ -91,12 +92,13 @@ const Contact = () => {
 					initial={{ y: "50%", opacity: 0, scale: 0.5 }}
 					animate={{ y: 0, opacity: 1, scale: 1 }}
 					transition={{ duration: 0.5, ease: "easeOut" }}
-					exit={{ opacity: 0, scale: 0.1 }}>
+					exit={{ opacity: 0, scale: 0.1 }}
+				>
 					{texts.confirm[lang]}
 				</motion.div>
 			)}
 		</div>
-	)
-}
+	);
+};
 
-export default Contact
+export default Contact;
