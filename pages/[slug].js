d5569894aae82e6349ab8800691456911f51e6ac
AmYre <benhammouda.amir@gmail.com>
2023-07-31 23:52:02 +0200

    fallback quality adapted

--- pages/[slug].js
+++ pages/[slug].js
@@ -145,6 +145,7 @@ const DetailProduct = () => {
 					poster={{ startOffset: "0" }}
 				>
 					<source src={`https://res.cloudinary.com/amircloud/video/upload/f_auto,q_auto/marc/${slug}-mob.mp4`} type="video/mp4" />
+					<source src={`https://res.cloudinary.com/amircloud/video/upload/f_auto:video,q_auto:low/marc/${slug}-mob.mp4`} type="video/mp4" />
 				</video>
 			),
 		[lang, slug, replay, isMobile]
